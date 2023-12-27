b74a4fa2700a9d640507bddc3837ace0adb4b48b
Trozza <troypoulter@gmail.com>
2023-12-27 09:49:43 +0000

    fix: conditionally add auth token for database connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,9 +5,12 @@ export default {
     schema: "./lib/db/schema.ts",
     out: "./lib/db/migrations",
     driver: "turso",
-    dbCredentials: {
+    dbCredentials: env.NODE_ENV === "production" ? {
         url: env.DATABASE_URL,
-        // authToken: env.DATABASE_AUTH_TOKEN
+        authToken: env.DATABASE_AUTH_TOKEN
+    } : {
+        url: env.DATABASE_URL
     },
     verbose: true,
+    strict: true
 } satisfies Config;

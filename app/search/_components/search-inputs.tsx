ecad5ca17ba13b57c76a05df8fd96594f699cbea
Trozza <troypoulter@gmail.com>
2023-12-30 05:18:55 +0000

    style: wrap search inputs if they overflow

--- app/search/_components/search-inputs.tsx
+++ app/search/_components/search-inputs.tsx
@@ -81,13 +81,14 @@ export const SearchInputs = () => {
         }
     }, [searchParams]);
 
+    // TODO: Move each input into its own component, maybe.
     return (
-        <div className="flex flex-row items-center justify-center gap-x-2">
+        <div className="flex flex-row flex-wrap items-center justify-center gap-x-4 gap-y-2">
             <Button disabled={loading} variant="outline" onClick={() => setGeolocationInURL()}>
                 {loading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : <LocateFixed className="mr-2 h-4 w-4" />} Use my location
             </Button>
             <Select value={fuelType} onValueChange={setFuelType}>
-                <SelectTrigger className="w-[180px]">
+                <SelectTrigger className="w-[160px]">
                     <SelectValue placeholder="Select a fuel type" />
                 </SelectTrigger>
                 <SelectContent>
@@ -101,14 +102,13 @@ export const SearchInputs = () => {
             </Select>
             <div className="grid gap-2 w-[160px]">
                 <div className="flex items-center justify-between">
-                    <Label htmlFor="radius">Radius (km)</Label>
+                    <Label htmlFor="radius">Radius</Label>
                     <span className="px-2 py-0.5 text-right text-sm text-muted-foreground">
-                        {radius}
+                        {radius}km
                     </span>
                 </div>
-                <Slider defaultValue={radius} onValueChange={setRadius} max={100} step={5} aria-label="radius" />
+                <Slider defaultValue={radius} onValueChange={setRadius} max={50} min={5} step={5} aria-label="radius" />
             </div>
-
         </div>
     )
 }

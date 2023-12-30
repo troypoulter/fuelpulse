e7de05a02e1873d962a2d5dc81e773b625ff5cb9
Trozza <troypoulter@gmail.com>
2023-12-30 05:11:19 +0000

    feat: add radius to search inputs

--- app/search/_components/search-inputs.tsx
+++ app/search/_components/search-inputs.tsx
@@ -3,6 +3,8 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider"
+import { Label } from "@/components/ui/label"
 import {
     Select,
     SelectContent,
@@ -25,6 +27,7 @@ export const SearchInputs = () => {
     const [latitude, setLatitude] = useState<string | undefined>(undefined);
     const [longitude, setLongitude] = useState<string | undefined>(undefined);
     const [fuelType, setFuelType] = useState("E10");
+    const [radius, setRadius] = useState([15]);
 
     const setGeolocationInURL = () => {
         setLoading(true);
@@ -79,7 +82,10 @@ export const SearchInputs = () => {
     }, [searchParams]);
 
     return (
-        <div className="flex items-center justify-center gap-x-2">
+        <div className="flex flex-row items-center justify-center gap-x-2">
+            <Button disabled={loading} variant="outline" onClick={() => setGeolocationInURL()}>
+                {loading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : <LocateFixed className="mr-2 h-4 w-4" />} Use my location
+            </Button>
             <Select value={fuelType} onValueChange={setFuelType}>
                 <SelectTrigger className="w-[180px]">
                     <SelectValue placeholder="Select a fuel type" />
@@ -93,9 +99,16 @@ export const SearchInputs = () => {
                     </SelectGroup>
                 </SelectContent>
             </Select>
-            <Button disabled={loading} variant="outline" onClick={() => setGeolocationInURL()}>
-                {loading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : <LocateFixed className="mr-2 h-4 w-4" />} Use my location
-            </Button>
+            <div className="grid gap-2 w-[160px]">
+                <div className="flex items-center justify-between">
+                    <Label htmlFor="radius">Radius (km)</Label>
+                    <span className="px-2 py-0.5 text-right text-sm text-muted-foreground">
+                        {radius}
+                    </span>
+                </div>
+                <Slider defaultValue={radius} onValueChange={setRadius} max={100} step={5} aria-label="radius" />
+            </div>
+
         </div>
     )
 }

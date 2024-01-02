ede5cb3385c4e22d778dd182379576476031c6d6
Trozza <troypoulter@gmail.com>
2024-01-02 09:39:33 +0000

    feat: add simple sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://fuelpulse.troypoulter.com',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: 'https://fuelpulse.troypoulter.com/search',
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 0.8,
+        }
+    ]
+}

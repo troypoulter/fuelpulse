4f45a3513c92589fb3b526888f3337c4f9f0ce99
Trozza <troypoulter@gmail.com>
2024-01-12 09:50:18 +0000

    chore: add copyright notice in footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
             <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
                 <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
                     <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                        Built by{" "}
+                        © 2023-{new Date().getFullYear()} Troy Poulter. Built by{" "}
                         <a
                             href="https://www.troypoulter.com/"
                             target="_blank"

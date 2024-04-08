5c5e42cc6b04118c4b86c8a9a09b4dc0702bf56c
troypoulter <troypoulter@gmail.com>
2024-04-09 07:55:35 +1000

    fix small bug with checking active navbar item

--- app/_components/sidebar-item.tsx
+++ app/_components/sidebar-item.tsx
@@ -25,7 +25,7 @@ export const SidebarItem = ({
   const isActive =
     (pathname === "/" && href === "/") ||
     pathname === href ||
-    pathname?.startsWith(`${href}/}`) ||
+    pathname?.startsWith(`${href}/`) ||
     (href === "/search" && pathname?.startsWith(`/stations/`));
 
   const onClick = () => {
